a6d12377240e6d6d0b3fe316c3cd8f3e89835ceb
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-08 02:51:25 +0530

    Update exceptions.py

--- plugins/exceptions.py
+++ plugins/exceptions.py
@@ -13,3 +13,12 @@ class DownloadError(Exception):
 class InvalidURL(Exception):
     """Raised when URL is invalid"""
     pass
+
+# Add the missing exceptions that are imported in scraper.py
+class SourceError(Exception):
+    """Raised when there's an error with an anime source"""
+    pass
+
+class NoResultsFound(Exception):
+    """Raised when no search results are found"""
+    pass
