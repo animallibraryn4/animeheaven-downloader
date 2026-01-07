22324e9d6c28c44bdffbfde2e7f5fe2306e13f00
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-08 01:33:01 +0530

    Delete src directory

--- src/exceptions.py
+++
@@ -1,7 +0,0 @@
-class DriverNotFound(Exception):
-    """Raised when Selenium driver cannot be found in system"""
-    pass
-
-class RequestBlocked(Exception):
-    """Rasied when Animeheaven blocked the request for abuse"""
-    pass
